80f7d097e4c7a3b4c3ed1b0dc87d95d57bbc13ad
jsp <jspenkar@gmail.com>
2014-10-30 00:22:08 -0500

    improvements to query

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,7 @@
 class RecipesController < ApplicationController
 	def index
-		@recipe = Recipe.all
+		@recipe_title = Recipe.all.map{|x| x.title}
+		@food = [1]
+		@recipe = Recipe
 	end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe
   include MongoMapper::Document
-  key :key, String
+  key :key, Fixnum
   key :title, String
   key :ingredient, String
   key :ingredient_array, String
