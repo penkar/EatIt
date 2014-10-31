afbd832d1f39cc7dd86fb509d11942f047f000e7
jsp <jspenkar@gmail.com>
2014-10-31 15:50:35 -0500

    Recipe match get complete.

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,5 +1,8 @@
+var recipeObject = {};
+var recipeListObject = [];
+var recipeSortedArray = []
+
 $(document).ready(function(){
-  var recipeObject = {};
 
   $('.submit-button').on('click', function(){
     recipeObject.stop = [];
@@ -8,12 +11,37 @@ $(document).ready(function(){
       if(array[i].checked){recipeObject.stop.push(array[i].value)}
     };
     recipeObject.go = $('#ingredientInput').val().split(',');
-    console.log(recipeObject);
+    recipeObject.name = $('#ex1_value').val();
   });
+
+  $('#aaaa').bind('ajax:success', function(){
+    recipeListObject = arguments[1]['recipe_list_relations'];
+    console.log(recipeListObject);
+    recipeSorter(recipeListObject);
+    $('form').toggle(500);
+
+  });
+
+  var recipeSorter = function(array){
+    recipeObject.displayOrder = []
+    var go = []
+    for(var i = 0; i < array.length; i++){
+      if((_.intersection(array[i].ingredient_array,recipeObject.stop).length === 0){
+      } else if((_.intersection(array[i].ingredient_array,recipeObject.go).length !== 0)){
+        recipeObject.displayOrder.push(array[i])
+      } else { go.push(array[i])}
+    }
+    _.flatten(recipeObject.displayOrder.push(go))
+  }
+
 });
 
-var app = angular.module('app', ["angucomplete"]);
 
+
+
+
+
+var app = angular.module('app', ["angucomplete"]);
 app.controller('MainController', ['$scope', '$http',
     function MainController($scope, $http) {
         $scope.titles = recipeTitles;
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,13 @@
 class RecipesController < ApplicationController
 	def index
+		@find = 'a'
 		@recipe_title = Recipe.all.map do |x| {name: x.title, ingredients: x.ingredient_array.map!{|x| x.capitalize} } end
-		@food = [1]
-		@recipe = Recipe
+	end
+
+	def find
+		title = params[:recipe_title]
+		match = eval(Recipe.all(:title => title).first.match).sort_by{|x,y| y}.map{|x| x[0].to_i}
+		recipes = match.map{|key| Recipe.all(:key => key).first}
+		render :json => { recipe_list_relations: recipes }
 	end
 end
