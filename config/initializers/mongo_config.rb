8851dc63d5d5bfb3267bb51ea7c1b1ace9176780
jsp <jspenkar@gmail.com>
2014-11-07 18:17:56 -0600

    commit to HEROKU!

--- config/initializers/mongo_config.rb
+++ config/initializers/mongo_config.rb
@@ -1,6 +1,16 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+# MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
 
-MongoMapper.database = "eat_it_app_#{Rails.env}"
-# MongoMapper.database = "appname-#{Rails.env}"
+# MongoMapper.database = "eat_it_app_#{Rails.env}"
+# # MongoMapper.database = "appname-#{Rails.env}"
 
-# MongoMapper.database = "eat_it_app_development"
+# # MongoMapper.database = "eat_it_app_development"
+
+
+if ENV["MONGOHQ_URL"]
+ uri = URI.parse(ENV["MONGOHQ_URL"])
+ MongoMapper.connection = Mongo::Connection.from_uri(ENV["MONGOHQ_URL"])
+ MongoMapper.database = uri.path.gsub(/^\//, '')
+else
+ MongoMapper.connection = Mongo::Connection.new('localhost')
+ MongoMapper.database = "eat_it_app_#{Rails.env}"
+end
