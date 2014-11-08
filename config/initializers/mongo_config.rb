92c66baf62db5a51800143921fc8a2d932b1d225
jsp <jspenkar@gmail.com>
2014-11-07 18:09:40 -0600

    More Changes for Heroku. Mongo config files.

--- config/initializers/mongo_config.rb
+++ config/initializers/mongo_config.rb
@@ -1,1 +1,6 @@
-MongoMapper.database = "eat_it_app_development"
+MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+
+MongoMapper.database = "eat_it_app_#{Rails.env}"
+# MongoMapper.database = "appname-#{Rails.env}"
+
+# MongoMapper.database = "eat_it_app_development"
